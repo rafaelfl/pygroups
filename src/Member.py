cf10314b5c7bcf43c2f98dd9d26afaf767682940
orobertojr <robertoaranhajr@gmail.com>
2019-07-02 18:51:20 -0300

    changing branch

--- src/Member.py
+++
@@ -1,3 +0,0 @@
-class Member (dict):
-    def __init__ (self, address, status):
-        super(Member, self).__init__((('address', list(address)), ('status', status)))
--- src/PyGroups-Client.py
+++
@@ -1,6 +0,0 @@
-from PyGroups import *
-group = PyGroup()
-group.callback(CMSG, utilities.default_msg)
-group.callback(JOIN, utilities.default_join)
-group.callback(LEAVE, utilities.default_leave)
-group.join("ECP")
